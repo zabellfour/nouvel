71abab6c6d77234851f51a1288cda6ce5bf8a244
zabellfour <zabell2814@gmail.com>
2018-08-01 23:46:42 +0300

    added header and hero section

---
+++ src/vendor_entries/vendor.js
@@ -0,0 +1,8 @@
+// All imports in this file will be compiled into vendors.js file.
+//
+// Note: ES6 support for these imports is not supported in base build
+
+module.exports = [
+  './node_modules/jquery/dist/jquery.js',
+  './node_modules/bootstrap/dist/js/bootstrap.min.js',
+];
