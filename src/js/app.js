94168b82afa4dbb04ac37402c4546ae018936c03
zabellfour <zabell2814@gmail.com>
2018-08-13 22:51:21 +0300

    fixed ios bug

--- src/js/app.js
+++ src/js/app.js
@@ -25,7 +25,38 @@ import DE from './modules/dots';
         }, false);
 
 
+        (function iOS_CaretBug() {
 
+            var ua = navigator.userAgent,
+            scrollTopPosition,
+            iOS = /iPad|iPhone|iPod/.test(ua),
+            iOS11 = /OS 11_0|OS 11_1|OS 11_2/.test(ua);
+    
+            // ios 11 bug caret position
+            if ( iOS && iOS11 ) {
+    
+                $(document.body).on('show.bs.modal', function(e) {
+                    if ( $(e.target).hasClass('inputModal') ) {
+                        // Get scroll position before moving top
+                        scrollTopPosition = $(document).scrollTop();
+    
+                        // Add CSS to body "position: fixed"
+                        $("body").addClass("iosBugFixCaret");
+                    }
+                });
+    
+                $(document.body).on('hide.bs.modal', function(e) {
+                    if ( $(e.target).hasClass('inputModal') ) {         
+                        // Remove CSS to body "position: fixed"
+                        $("body").removeClass("iosBugFixCaret");
+    
+                        //Go back to initial position in document
+                        $(document).scrollTop(scrollTopPosition);
+                    }
+                });
+    
+            }
+        })();
 
         function AjaxFormRequest(result_id, formMain, url) {
 
